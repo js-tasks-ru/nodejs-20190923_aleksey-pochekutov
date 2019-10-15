2d09044c43c26d818402580ba8d1e25f8be45349
Alexey Pochekutov <pochekutov.a.d@sberbank.ru>
2019-10-15 08:16:55 +0300

    add solution for 4-module 3-task

--- 4-module/3-task/server.js
+++ 4-module/3-task/server.js
@@ -1,6 +1,7 @@
 const url = require('url');
 const http = require('http');
 const path = require('path');
+const fs = require('fs');
 
 const server = new http.Server();
 
@@ -11,6 +12,21 @@ server.on('request', (req, res) => {
 
   switch (req.method) {
     case 'DELETE':
+
+      req.on('error', () => {
+        res.statusCode = 500;
+        res.end('internal error');
+      });
+
+      fs.unlink(filepath, (err) => {
+        if (!err) {
+          res.statusCode = 200;
+          res.end('OK');
+        } else {
+          res.statusCode = 404;
+          res.end('file is not define');
+        }
+      });
 
       break;
 
