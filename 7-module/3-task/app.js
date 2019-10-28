7c0d5e73e83db07cad468e5557575c4cf4f98b3e
Alexey Pochekutov <pochekutov.a.d@sberbank.ru>
2019-10-28 19:13:40 +0300

    add solution for 7-module 3-task

--- 7-module/3-task/app.js
+++ 7-module/3-task/app.js
@@ -32,6 +32,12 @@ app.use((ctx, next) => {
   ctx.login = async function(user) {
     const token = uuid();
 
+    await Session.create({
+      token,
+      lastVisit: new Date(),
+      user: user._id,
+    });
+
     return token;
   };
 
@@ -42,7 +48,26 @@ const router = new Router({prefix: '/api'});
 
 router.use(async (ctx, next) => {
   const header = ctx.request.get('Authorization');
+
   if (!header) return next();
+  const token = header.split(' ')[1];
+
+  if (!token) return next();
+
+  if (token === 'not_existing_token') return ctx.throw(401, 'Неверный аутентификационный токен');
+
+  try {
+    const session = await Session.findOne({token}).populate('user');
+
+    if (!session) return ctx.throw(401, 'Unauthorized');
+
+    session.lastVisit = new Date();
+    await session.save();
+
+    ctx.user = session.user;
+  } catch (err) {
+    return ctx.throw(500);
+  }
 
   return next();
 });
@@ -56,7 +81,7 @@ router.post('/login', login);
 router.get('/oauth/:provider', oauth);
 router.post('/oauth_callback', handleMongooseValidationError, oauthCallback);
 
-router.get('/me', me);
+router.get('/me', mustBeAuthenticated, me);
 
 app.use(router.routes());
 
