ed0f2b659e62a6cd95a38d4ddf6cbc6cca91018b
alexeyerpd <alexeyerpd@gmail.com>
2019-11-04 19:17:44 +0300

    fix 8-module 1-task

--- 8-module/1-task/controllers/registration.js
+++ 8-module/1-task/controllers/registration.js
@@ -16,6 +16,10 @@ module.exports.register = async (ctx, next) => {
   const {email, displayName, password} = ctx.request.body;
   if (!email || !displayName || !password) return ctx.throw(400, 'Вы должны ввести все данные');
 
+  const u = await User.findOne({email});
+
+  if (u) return ctx.throw(400, `{ errors: { email: 'Такой email уже существует' } }`);
+
   try {
     const u = await User.create({
       email,
@@ -33,6 +37,7 @@ module.exports.register = async (ctx, next) => {
     });
     ctx.body = 'письмо отправлено на указанный email';
   } catch (err) {
+    console.log(err);
     if (err.name === 'ValidationError') {
       const error = getErrors(err);
       return ctx.throw(400, JSON.stringify(error));
@@ -41,6 +46,36 @@ module.exports.register = async (ctx, next) => {
     return ctx.throw(500);
   }
 };
+// module.exports.register = async (ctx, next) => {
+//   const {email, displayName, password} = ctx.request.body;
+//   if (!email || !displayName || !password) return ctx.throw(400, 'Вы должны ввести все данные');
+
+//   try {
+//     const u = await User.create({
+//       email,
+//       displayName,
+//       verificationToken: uuid(),
+//     });
+//     await u.setPassword(password);
+//     await u.save();
+
+//     await sendMail({
+//       template: 'confirmation',
+//       locals: {token: u.verificationToken},
+//       to: email,
+//       subject: 'Подтвердите почту',
+//     });
+//     ctx.body = 'письмо отправлено на указанный email';
+//   } catch (err) {
+//     console.log(err);
+//     if (err.name === 'ValidationError') {
+//       const error = getErrors(err);
+//       return ctx.throw(400, JSON.stringify(error));
+//     }
+
+//     return ctx.throw(500);
+//   }
+// };
 
 module.exports.confirm = async (ctx, next) => {
   const {verificationToken} = ctx.request.body;
