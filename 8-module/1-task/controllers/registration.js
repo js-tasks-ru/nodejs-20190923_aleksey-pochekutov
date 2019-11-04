c7cbe946ea81f1bc281e18f0c1f62c00d3ec5af6
alexeyerpd <alexeyerpd@gmail.com>
2019-11-04 18:33:30 +0300

    add solution for 8-module 1-task

--- 8-module/1-task/controllers/registration.js
+++ 8-module/1-task/controllers/registration.js
@@ -1,9 +1,53 @@
 const uuid = require('uuid/v4');
 const User = require('../models/User');
-const sendMail = require('../libs/sendMail');
+// const sendMail = require('../libs/sendMail');
+
+function getErrors(errorObj) {
+  const {errors} = errorObj;
+  const err = Object.create(null);
+  const keys = Object.keys(errors);
+
+  keys.forEach((key) => err[key] = errors[key].message);
+
+  return {errors: err};
+}
 
 module.exports.register = async (ctx, next) => {
+  const {email, displayName, password} = ctx.request.body;
+  if (!email || !displayName || !password) return ctx.throw(400, 'Вы должны ввести все данные');
+
+  try {
+    const u = await User.create({
+      email,
+      displayName,
+      verificationToken: uuid(),
+    });
+    await u.setPassword(password);
+    await u.save();
+    ctx.body = 'письмо отправлено на указанный email';
+  } catch (err) {
+    if (err.name === ' ValidationError') {
+      const error = getErrors(err);
+      return ctx.throw(400, JSON.stringify(error));
+    }
+
+    return ctx.throw(500);
+  }
 };
 
 module.exports.confirm = async (ctx, next) => {
+  const {verificationToken} = ctx.request.body;
+
+  if (!verificationToken) return ctx.throw(400, 'Empty token');
+
+  const u = await User.findOne({verificationToken});
+
+  if (!u) return ctx.throw(400, 'Ссылка подтверждения недействительна или устарела');
+
+  u.verificationToken = undefined;
+  await u.save();
+
+  const token = await ctx.login(u._id);
+
+  ctx.body = {token};
 };
